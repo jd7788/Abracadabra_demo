e4b1d02e28ffacaec02c9491697e3d3fdff8fe0b
SheepChef <wuyuyang2007@gmail.com>
2024-11-23 15:06:41 +0800

    Update Demo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import App from "./App.vue";
 import { setColorScheme } from "mdui/functions/setColorScheme.js";
 import Card from "./components/MdCard.vue";
 
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  window.deferredPrompt = e;
+});
+
 const app = createApp(App);
 setColorScheme("#09355b");
 app.component("Card", Card);
