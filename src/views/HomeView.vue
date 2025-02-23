64c8131f7e665f020ee37c2eb81400f9bf26b314
SheepChef <wuyuyang2007@gmail.com>
2025-02-23 21:25:39 +0800

    Update V3.0.8

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -596,7 +596,7 @@ onBeforeUnmount(() => {});
             margin: 0px;
           "
         >
-          Abracadabra V3.0.5<br /><a href="https://github.com/SheepChef/Abracadabra">Github Repo</a>
+          Abracadabra V3.0.8<br /><a href="https://github.com/SheepChef/Abracadabra">Github Repo</a>
         </p>
         <mdui-chip
           v-if="ShowPWAButton"
